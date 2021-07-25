1527f15e74852408586fba9e771b244c7c90897e
Rakesh <rakesh780@gmail.com>
2021-07-25 10:53:45 +0530

    added basic frame and apis
    Signed-off-by: Rakesh <rakesh780@gmail.com>

---
+++ src/main/java/com/api/SpringBootSecurityJwtMongodbApplication.java
@@ -0,0 +1,22 @@
+package com.api;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootSecurityJwtMongodbApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityJwtMongodbApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+	{
+		return builder.sources(SpringBootSecurityJwtMongodbApplication.class);
+	}
+
+}
